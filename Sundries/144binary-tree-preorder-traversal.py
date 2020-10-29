ca0ef19465c57eb7d53e77a44b908a727e1bd1f3
Payne <1289690157@qq.com>
2020-10-29 10:10:10 +0800

    add binary tree preorder

---
+++ Sundries/144binary-tree-preorder-traversal.py
@@ -0,0 +1,38 @@
+# Part 1 recursion
+from typing import List
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution1:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        def preorder(root: TreeNode):
+            if not root:
+                return
+            res.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        res = list()
+        preorder(root)
+        return res
+
+# Part 2 stack
+
+
+class Solution2:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        ret = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                ret.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return ret
